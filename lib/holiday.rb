0daa74a348f8b702acf5fbaea91a49f8c7e66705
Erin Walker <erinb0317@hotmail.com>
2018-02-01 01:41:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,9 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts "#{season.to_s.capitalize!}:"
+    puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday_name, supplies|
-      holidayay = holiday.to_s.split("_").map{|word| word.capitalize}.join(" ")
+      holidayay = holiday_name.to_s.split("_").map{|word| word.capitalize}.join(" ")
       puts "#{holidayay}: #{supplies.join(", ")}"
     end
   end
